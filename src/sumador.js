3c927b28c78506ffa96137c6094a1a0e5967556e
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-18 18:00:57 -0400

    Agregacion de test unitarios

--- src/sumador.js
+++
@@ -1,8 +0,0 @@
-//////////////////        D E M O   //////////////////
-function sumar(a, b) {
-  return a + b;
-}
-
-export default sumar;
-
-//////////////////        P R O Y E C T O   //////////////////
